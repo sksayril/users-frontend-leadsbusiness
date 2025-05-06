45a8495c0a144a179c01bba902bb8d1e73dc9f08
sksayril <sksayril123@gmail.com>
2025-05-06 21:24:49 +0530

    chnages

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 
-const API_BASE_URL = 'https://api.datahive.co.in/api';
+const API_BASE_URL = 'https://7cvccltb-3600.inc1.devtunnels.ms/api';
 
 // Create an Axios instance with default configuration
 const api = axios.create({
